d538057df12d90980886891d9d545e37dee198cb
schitt387 <schitt387@gmail.com>
2021-06-25 16:31:46 +0530

    File upload and preview

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import "antd/dist/antd.css"
+import Uploader from './components/uploader'
+
+function App() {
+  return (
+    <div className="App">
+     <Uploader/>
+    </div>
+  );
+}
+
+export default App;
